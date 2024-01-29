90fa0cc40a9fb943f50570dcbdaddcc70724571b
Cameron Cove-Crichton <116748981+CamCove-Crichton@users.noreply.github.com>
2024-01-29 14:42:37 +0000

    Install django-decouple and update requirements.txt, add API configs, adjust gitignore file and update README

--- lfms/workload/views.py
+++ lfms/workload/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
+from django.conf import settings
+import requests
 
 def workload(request):
     """
     A view to display the current workload and upcoming
     workload using in the form of a traffic light system
     """
+    # API configurations
+    url = settings.API_URL
+    subdomain = settings.X_SUBDOMAIN
+    auth_token = settings.X_AUTH_TOKEN
+
+    payload = {}
+
+    headers = {
+        'X-SUBDOMAIN': subdomain,
+        'X-AUTH-TOKEN': auth_token,
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text)
+    
     template = 'workload/workload.html'
     # context = {}
 
