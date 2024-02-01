bb1960e968f36696eaa4887be084fd0230ac2585
Cameron Cove-Crichton <116748981+CamCove-Crichton@users.noreply.github.com>
2024-02-01 15:38:02 +0000

    Add api call to api_calls.py, create utils.py for additional funtions, get test function for weight total of jobs in workloads views.py

--- lfms/workload/views.py
+++ lfms/workload/views.py
@@ -1,43 +1,45 @@
 from django.shortcuts import render
-from django.conf import settings
-import requests
+from .api_calls import get_opportunities
+from .utils import round_to_decimal
 
 def workload(request):
     """
     A view to display the current workload and upcoming
     workload using in the form of a traffic light system
     """
-    # API configurations
-    url = settings.API_URL
-    subdomain = settings.X_SUBDOMAIN
-    auth_token = settings.X_AUTH_TOKEN
+    provisional_opportunities = get_opportunities(page=1, per_page=25, state_eq=2, status_eq=1)
+    reserved_opportunities = get_opportunities(page=1, per_page=25, state_eq=2, status_eq=5)
+    confirmed_opportunities = get_opportunities(page=1, per_page=25, state_eq=3, status_eq=0)
 
-    payload = {}
+    # print(reserved_opportunities)
 
-    headers = {
-        'X-SUBDOMAIN': subdomain,
-        'X-AUTH-TOKEN': auth_token,
+    weights = []
+
+    for opportunity in provisional_opportunities['opportunities']:
+        weight_total_str = opportunity['weight_total']
+        
+        try:
+            #Try convert the string to a float
+            weight_total = float(weight_total_str)
+            weight_total_rounded = round_to_decimal(weight_total, 2)
+            weights.append(weight_total_rounded)
+        except ValueError:
+            #Handle the case where the string is not a float
+            print(f'Warning: Could not convert {weight_total_str} to a float')
+    
+    print(weights)
+
+    total_weight = sum(weights)
+
+    print(f'Total weight: {total_weight}')
+    
+    
+
+    template = 'workload/workload.html'
+    context = {
+        'provisional_opportunities': provisional_opportunities,
+        'reserved_opportunities': reserved_opportunities,
+        'confirmed_opportunities': confirmed_opportunities,
     }
 
-    response = requests.request("GET", url, headers=headers, data=payload)
-
-    print(response.text)
-
-    if response.status_code == 200:
-        data = response.json()
-
-        order_opportunities = {}
-
-        for opportunity in data['opportunities']:
-            if opportunity.get('state_name') == 'Order':
-                order_opportunities[opportunity['id']] = opportunity
-            
-        # print(order_opportunities)
-    else:
-        print(f'Error: {response.status_code}')
-
-    
-    template = 'workload/workload.html'
-    # context = {}
-
-    return render(request, template)
+    return render(request, template, context)
