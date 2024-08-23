efe388f484b31e442baca1ff4cf1ce79bcf07f8d
Cameron Crichton <cameroncrichton@Cameron-Cove-Crichton-3.local>
2024-08-23 10:20:07 +0100

    Update email backend setting in settings.py

--- lfms/settings.py
+++ lfms/settings.py
@@ -170,7 +170,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Email configurations
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
