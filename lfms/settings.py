1b61ce9227af246f6989906eb690462ab5dbbfd7
Cameron Cove-Crichton <cameroncove-crichton@Camerons-MacBook-Air.local>
2025-03-23 15:19:03 +0000

    Update celery config

--- lfms/settings.py
+++ lfms/settings.py
@@ -183,7 +183,7 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 
 # Celery Configuration
-CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_BROKER_URL = os.getenv('UPSTASH_REDIS_URL', 'redis://localhost:6379/0')
 CELERY_RESULT_BACKEND = "django-db"
 app = Celery('lfms')
 app.config_from_object('django.conf:settings', namespace='CELERY')
