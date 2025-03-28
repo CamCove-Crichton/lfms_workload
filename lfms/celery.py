6f220a65d8affbcda6bfa59c56bff01e818340e0
Cameron Cove-Crichton <cameroncove-crichton@Camerons-MacBook-Air.local>
2025-03-28 05:36:36 +0000

    Update SSL settings for celery

--- lfms/celery.py
+++ lfms/celery.py
@@ -9,15 +9,15 @@ app = Celery('lfms')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# USE_CELERY_SSL = os.getenv('USE_CELERY_SSL', 'False').lower() == 'true'
+USE_CELERY_SSL = os.getenv('USE_CELERY_SSL', 'False').lower() == 'true'
 
-# if USE_CELERY_SSL:
-#     app.conf.update(
-#         broker_use_ssl={
-#             'ssl_cert_reqs': 'CERT_NONE',  # Adjust based on requirements
-#         },
-#         broker_connection_retry_on_startup=True,
-#     )
+if USE_CELERY_SSL:
+    app.conf.update(
+        broker_use_ssl={
+            'ssl_cert_reqs': 'CERT_NONE',  # Adjust based on requirements
+        },
+        broker_connection_retry_on_startup=True,
+    )
 
 
 # Auto-discover tasks in all Django apps
