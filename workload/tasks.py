d2c976d6c0b8cbb3190b42bd5e0b8abf677b440b
Cameron Cove-Crichton <cameroncove-crichton@Camerons-MacBook-Air.local>
2025-05-10 15:41:15 +0100

    Update confirmed order background colour and add logger info to fetch_workshop_workload function

--- workload/tasks.py
+++ workload/tasks.py
@@ -1,9 +1,13 @@
 from celery import shared_task
 from .utils import fetch_workload_data
+import logging
+
+logger = logging.getLogger(__name__)
 
 @shared_task
 def fetch_workshop_workload(days):
     """Celery task to fetch workload data asynchronously."""
+    logger.info(f"Running Celery Task with days={days}")
     data = fetch_workload_data(days=days)
     
     return data
