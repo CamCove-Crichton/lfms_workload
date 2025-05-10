e6dd85c948701198733fbc4bb0289aafdd0e555e
Cameron Cove-Crichton <cameroncove-crichton@Camerons-MacBook-Air.local>
2025-05-10 16:50:55 +0100

    Update celery result backend and add print statement to check celery is loading

--- lfms/celery.py
+++ lfms/celery.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
+print("✅ Celery is loading...")
+
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lfms.settings')
 
