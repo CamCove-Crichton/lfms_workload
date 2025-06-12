c15a71584ba40ed8f7b6c707bc1d231e9f9f037f
Cameron Cove-Crichton <cameroncove-crichton@Camerons-MacBook-Air.local>
2025-06-12 06:26:58 +0100

    Add additional logging for debugging

--- workload/admin.py
+++ workload/admin.py
@@ -1,4 +1,3 @@
 from django.contrib import admin
-from django_celery_results.models import TaskResult
 
-admin.site.register(TaskResult)
+# Register your models here.
--- workload/apps.py
+++ workload/apps.py
@@ -1,6 +1,17 @@
 from django.apps import AppConfig
+import logging
 
 
 class WorkloadConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "workload"
+
+    def ready(self):
+        from celery.signals import task_success
+        from django_celery_results.models import TaskResult
+
+        logger = logging.getLogger(__name__)
+
+        @task_success.connect
+        def task_succeeded_handler(sender=None, result=None, **kwargs):
+            logger.warning(f"[Celery Signal] Task {sender.request.id} succeeded with result: {result}")
